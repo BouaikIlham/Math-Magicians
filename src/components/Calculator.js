c05dc1e39cc50b1662edda3e8457a3bbc6d78e14
ilham bouaik <ibouaik@gmail.com>
2022-06-01 13:12:42 +0100

    add events to button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,44 +1,111 @@
 import React from 'react';
 import './style.css';
+import calculate from '../logic/calculate';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      obj: {
+        total: null,
+        next: null,
+        operation: null,
+      },
+    };
+    this.handlerclick = this.handlerclick.bind(this);
+  }
+
+  handlerclick(buttonName) {
+    const { obj } = this.state;
+    this.setState({ obj: calculate(obj, buttonName) });
+  }
+
   render() {
+    const { obj } = this.state;
     return (
       <>
         <div className="calculater-grid">
-          <div className="previous-operand">
-            <input className="value" type="text" />
-          </div>
-          <button type="submit">AC</button>
-          <button type="submit">+/-</button>
-          <button type="submit">%</button>
-          <button className="calculations" type="submit">
+          <div className="previous-operand">{obj.next || obj.total || 0}</div>
+          <button type="submit" onClick={() => this.handlerclick('AC')}>
+            AC
+          </button>
+          <button type="submit" onClick={() => this.handlerclick('+/-')}>
+            +/-
+          </button>
+          <button type="submit" onClick={() => this.handlerclick('%')}>
+            %
+          </button>
+          <button
+            className="calculations"
+            type="submit"
+            onClick={() => this.handlerclick('÷')}
+          >
             ÷
           </button>
-          <button type="submit">7</button>
-          <button type="submit">8</button>
-          <button type="submit">9</button>
-          <button className="calculations" type="submit">
+          <button type="submit" onClick={() => this.handlerclick('7')}>
+            7
+          </button>
+          <button type="submit" onClick={() => this.handlerclick('8')}>
+            8
+          </button>
+          <button type="submit" onClick={() => this.handlerclick('9')}>
+            9
+          </button>
+          <button
+            className="calculations"
+            type="submit"
+            onClick={() => this.handlerclick('×')}
+          >
             ×
           </button>
-          <button type="submit">4</button>
-          <button type="submit">5</button>
-          <button type="submit">6</button>
-          <button className="calculations" type="submit">
+          <button type="submit" onClick={() => this.handlerclick('4')}>
+            4
+          </button>
+          <button type="submit" onClick={() => this.handlerclick('5')}>
+            5
+          </button>
+          <button type="submit" onClick={() => this.handlerclick('6')}>
+            6
+          </button>
+          <button
+            className="calculations"
+            type="submit"
+            onClick={() => this.handlerclick('-')}
+          >
             -
           </button>
-          <button type="submit">1</button>
-          <button type="submit">2</button>
-          <button type="submit">3</button>
-          <button className="calculations" type="submit">
+          <button type="submit" onClick={() => this.handlerclick('1')}>
+            1
+          </button>
+          <button type="submit" onClick={() => this.handlerclick('2')}>
+            2
+          </button>
+          <button type="submit" onClick={() => this.handlerclick('3')}>
+            3
+          </button>
+          <button
+            className="calculations"
+            type="submit"
+            onClick={() => this.handlerclick('+')}
+          >
             +
           </button>
-          <button className="number-0" type="submit">
+          <button
+            className="number-0"
+            type="submit"
+            onClick={() => this.handlerclick('0')}
+          >
             0
           </button>
-          <button type="submit">.</button>
-          <button className="calculations" type="submit">
+          <button type="submit" onClick={() => this.handlerclick('.')}>
+            .
+          </button>
+          <button
+            className="calculations"
+            type="submit"
+            onClick={() => this.handlerclick('=')}
+          >
             =
           </button>
         </div>

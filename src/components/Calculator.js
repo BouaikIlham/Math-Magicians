f84dba02330ef64a4dbebccc10adcdb73dbd592c
ilham bouaik <ibouaik@gmail.com>
2022-06-07 18:54:46 +0100

    add 3 section

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -14,92 +14,96 @@ function Calculator() {
   const handlerclick = (buttonName) => setState(({ obj }) => ({ obj: calculate(obj, buttonName) }));
   return (
     <>
-      <div className="calculater-grid">
-        <div className="previous-operand">
-          {state.obj.next || state.obj.total || 0}
+      <section className="calculator">
+        <h1 className="Title">Lets do some Math !!</h1>
+        <div className="calculater-grid">
+          <div className="previous-operand">
+            {state.obj.next || state.obj.total || 0}
+          </div>
+          <button type="submit" onClick={() => handlerclick('AC')}>
+            AC
+          </button>
+          <button type="submit" onClick={() => handlerclick('+/-')}>
+            +/-
+          </button>
+          <button type="submit" onClick={() => handlerclick('%')}>
+            %
+          </button>
+          <button
+            className="calculations"
+            type="submit"
+            onClick={() => handlerclick('÷')}
+          >
+            ÷
+          </button>
+          <button type="submit" onClick={() => handlerclick('7')}>
+            7
+          </button>
+          <button type="submit" onClick={() => handlerclick('8')}>
+            8
+          </button>
+          <button type="submit" onClick={() => handlerclick('9')}>
+            9
+          </button>
+          <button
+            className="calculations"
+            type="submit"
+            onClick={() => handlerclick('x')}
+          >
+            x
+          </button>
+          <button type="submit" onClick={() => handlerclick('4')}>
+            4
+          </button>
+          <button type="submit" onClick={() => handlerclick('5')}>
+            5
+          </button>
+          <button type="submit" onClick={() => handlerclick('6')}>
+            6
+          </button>
+          <button
+            className="calculations"
+            type="submit"
+            onClick={() => handlerclick('-')}
+          >
+            -
+          </button>
+          <button type="submit" onClick={() => handlerclick('1')}>
+            1
+          </button>
+          <button type="submit" onClick={() => handlerclick('2')}>
+            2
+          </button>
+          <button type="submit" onClick={() => handlerclick('3')}>
+            3
+          </button>
+          <button
+            className="calculations"
+            type="submit"
+            onClick={() => handlerclick('+')}
+          >
+            +
+          </button>
+          <button
+            className="number-0"
+            type="submit"
+            onClick={() => handlerclick('0')}
+          >
+            0
+          </button>
+          <button type="submit" onClick={() => handlerclick('.')}>
+            .
+          </button>
+          <button
+            className="calculations"
+            type="submit"
+            onClick={() => handlerclick('=')}
+          >
+            =
+          </button>
         </div>
-        <button type="submit" onClick={() => handlerclick('AC')}>
-          AC
-        </button>
-        <button type="submit" onClick={() => handlerclick('+/-')}>
-          +/-
-        </button>
-        <button type="submit" onClick={() => handlerclick('%')}>
-          %
-        </button>
-        <button
-          className="calculations"
-          type="submit"
-          onClick={() => handlerclick('÷')}
-        >
-          ÷
-        </button>
-        <button type="submit" onClick={() => handlerclick('7')}>
-          7
-        </button>
-        <button type="submit" onClick={() => handlerclick('8')}>
-          8
-        </button>
-        <button type="submit" onClick={() => handlerclick('9')}>
-          9
-        </button>
-        <button
-          className="calculations"
-          type="submit"
-          onClick={() => handlerclick('x')}
-        >
-          x
-        </button>
-        <button type="submit" onClick={() => handlerclick('4')}>
-          4
-        </button>
-        <button type="submit" onClick={() => handlerclick('5')}>
-          5
-        </button>
-        <button type="submit" onClick={() => handlerclick('6')}>
-          6
-        </button>
-        <button
-          className="calculations"
-          type="submit"
-          onClick={() => handlerclick('-')}
-        >
-          -
-        </button>
-        <button type="submit" onClick={() => handlerclick('1')}>
-          1
-        </button>
-        <button type="submit" onClick={() => handlerclick('2')}>
-          2
-        </button>
-        <button type="submit" onClick={() => handlerclick('3')}>
-          3
-        </button>
-        <button
-          className="calculations"
-          type="submit"
-          onClick={() => handlerclick('+')}
-        >
-          +
-        </button>
-        <button
-          className="number-0"
-          type="submit"
-          onClick={() => handlerclick('0')}
-        >
-          0
-        </button>
-        <button type="submit" onClick={() => handlerclick('.')}>
-          .
-        </button>
-        <button
-          className="calculations"
-          type="submit"
-          onClick={() => handlerclick('=')}
-        >
-          =
-        </button>
-      </div>
+      </section>
+
     </>
   );
 }
